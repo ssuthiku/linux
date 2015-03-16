7d9f33f1d8ec42114fca20f3f97a9301840435ca
Tomasz Nowicki <tomasz.nowicki@linaro.org>
2015-03-16 14:00:38 +0100

    pci, ecam: Improve naming for ecam.c content and areas where it is used.
    This commit is consequence of making x86 MMCONFIG code available for all
    architectures. Current code has old name fashion so for the sake of
    making it more understandable and express where it comes from,
    we are changing functions/variables/macros etc. name *mmconfig* -> *ecam*
    
    Signed-off-by: Tomasz Nowicki <tomasz.nowicki@linaro.org>

--- drivers/pci/ecam.c
+++ drivers/pci/ecam.c
@@ -17,24 +17,24 @@
 
 #include <asm/io.h>
 
-#define PREFIX "PCI: "
+#define PREFIX "PCI ECAM: "
 
-static DEFINE_MUTEX(pci_mmcfg_lock);
+static DEFINE_MUTEX(pci_ecam_lock);
 
-LIST_HEAD(pci_mmcfg_list);
+LIST_HEAD(pci_ecam_list);
 
 #ifdef CONFIG_GENERIC_PCI_ECAM
 static char __iomem *pci_dev_base(unsigned int seg, unsigned int bus,
 				  unsigned int devfn)
 {
-	struct pci_mmcfg_region *cfg = pci_mmconfig_lookup(seg, bus);
+	struct pci_ecam_region *cfg = pci_ecam_lookup(seg, bus);
 
 	if (cfg && cfg->virt)
-		return cfg->virt + (PCI_MMCFG_BUS_OFFSET(bus) | (devfn << 12));
+		return cfg->virt + (PCI_ECAM_BUS_OFFSET(bus) | (devfn << 12));
 	return NULL;
 }
 
-int pci_mmcfg_read(unsigned int seg, unsigned int bus,
+int pci_ecam_read(unsigned int seg, unsigned int bus,
 			  unsigned int devfn, int reg, int len, u32 *value)
 {
 	char __iomem *addr;
@@ -58,7 +58,7 @@ err:		*value = -1;
 	return 0;
 }
 
-int pci_mmcfg_write(unsigned int seg, unsigned int bus,
+int pci_ecam_write(unsigned int seg, unsigned int bus,
 			   unsigned int devfn, int reg, int len, u32 value)
 {
 	char __iomem *addr;
@@ -80,64 +80,64 @@ int pci_mmcfg_write(unsigned int seg, unsigned int bus,
 	return 0;
 }
 
-static void __iomem *mcfg_ioremap(struct pci_mmcfg_region *cfg)
+static void __iomem *pci_ecam_ioremap(struct pci_ecam_region *cfg)
 {
 	void __iomem *addr;
 	u64 start, size;
 	int num_buses;
 
-	start = cfg->address + PCI_MMCFG_BUS_OFFSET(cfg->start_bus);
+	start = cfg->address + PCI_ECAM_BUS_OFFSET(cfg->start_bus);
 	num_buses = cfg->end_bus - cfg->start_bus + 1;
-	size = PCI_MMCFG_BUS_OFFSET(num_buses);
+	size = PCI_ECAM_BUS_OFFSET(num_buses);
 	addr = ioremap_nocache(start, size);
 	if (addr)
-		addr -= PCI_MMCFG_BUS_OFFSET(cfg->start_bus);
+		addr -= PCI_ECAM_BUS_OFFSET(cfg->start_bus);
 	return addr;
 }
 
-int __init pci_mmcfg_arch_init(void)
+int __init pci_ecam_arch_init(void)
 {
-	struct pci_mmcfg_region *cfg;
+	struct pci_ecam_region *cfg;
 
-	list_for_each_entry(cfg, &pci_mmcfg_list, list)
-		if (pci_mmcfg_arch_map(cfg)) {
-			pci_mmcfg_arch_free();
+	list_for_each_entry(cfg, &pci_ecam_list, list)
+		if (pci_ecam_arch_map(cfg)) {
+			pci_ecam_arch_free();
 			return 0;
 		}
 
 	return 1;
 }
 
-void __init pci_mmcfg_arch_free(void)
+void __init pci_ecam_arch_free(void)
 {
-	struct pci_mmcfg_region *cfg;
+	struct pci_ecam_region *cfg;
 
-	list_for_each_entry(cfg, &pci_mmcfg_list, list)
-		pci_mmcfg_arch_unmap(cfg);
+	list_for_each_entry(cfg, &pci_ecam_list, list)
+		pci_ecam_arch_unmap(cfg);
 }
 
-int pci_mmcfg_arch_map(struct pci_mmcfg_region *cfg)
+int pci_ecam_arch_map(struct pci_ecam_region *cfg)
 {
-	cfg->virt = mcfg_ioremap(cfg);
+	cfg->virt = pci_ecam_ioremap(cfg);
 	if (!cfg->virt) {
-		pr_err(PREFIX "can't map MMCONFIG at %pR\n", &cfg->res);
+		pr_err(PREFIX "can't map ECAM at %pR\n", &cfg->res);
 		return -ENOMEM;
 	}
 
 	return 0;
 }
 
-void pci_mmcfg_arch_unmap(struct pci_mmcfg_region *cfg)
+void pci_ecam_arch_unmap(struct pci_ecam_region *cfg)
 {
 	if (cfg && cfg->virt) {
-		iounmap(cfg->virt + PCI_MMCFG_BUS_OFFSET(cfg->start_bus));
+		iounmap(cfg->virt + PCI_ECAM_BUS_OFFSET(cfg->start_bus));
 		cfg->virt = NULL;
 	}
 }
 #endif
 
 static u32
-pci_mmconfig_generic_read(int len, void __iomem *addr)
+pci_ecam_generic_read(int len, void __iomem *addr)
 {
 	u32 data = 0;
 
@@ -157,7 +157,7 @@ pci_mmconfig_generic_read(int len, void __iomem *addr)
 }
 
 static void
-pci_mmconfig_generic_write(int len, void __iomem *addr, u32 value)
+pci_ecam_generic_write(int len, void __iomem *addr, u32 value)
 {
 	switch (len) {
 	case 1:
@@ -172,42 +172,42 @@ pci_mmconfig_generic_write(int len, void __iomem *addr, u32 value)
 	}
 }
 
-static struct pci_mmcfg_mmio_ops pci_mmcfg_mmio_default = {
-	.read = pci_mmconfig_generic_read,
-	.write = pci_mmconfig_generic_write,
+static struct pci_ecam_mmio_ops pci_ecam_mmio_default = {
+	.read = pci_ecam_generic_read,
+	.write = pci_ecam_generic_write,
 };
 
-static struct pci_mmcfg_mmio_ops *pci_mmcfg_mmio = &pci_mmcfg_mmio_default;
+static struct pci_ecam_mmio_ops *pci_ecam_mmio = &pci_ecam_mmio_default;
 
 void
-pci_mmconfig_register_mmio(struct pci_mmcfg_mmio_ops *ops)
+pci_ecam_register_mmio(struct pci_ecam_mmio_ops *ops)
 {
-	pci_mmcfg_mmio = ops;
+	pci_ecam_mmio = ops;
 }
 
 u32
 pci_mmio_read(int len, void __iomem *addr)
 {
-	if (!pci_mmcfg_mmio) {
+	if (!pci_ecam_mmio) {
 		pr_err("PCI config space has no accessors !");
 		return 0;
 	}
 
-	return pci_mmcfg_mmio->read(len, addr);
+	return pci_ecam_mmio->read(len, addr);
 }
 
 void
 pci_mmio_write(int len, void __iomem *addr, u32 value)
 {
-	if (!pci_mmcfg_mmio) {
+	if (!pci_ecam_mmio) {
 		pr_err("PCI config space has no accessors !");
 		return;
 	}
 
-	pci_mmcfg_mmio->write(len, addr, value);
+	pci_ecam_mmio->write(len, addr, value);
 }
 
-static void __init pci_mmconfig_remove(struct pci_mmcfg_region *cfg)
+static void __init pci_ecam_remove(struct pci_ecam_region *cfg)
 {
 	if (cfg->res.parent)
 		release_resource(&cfg->res);
@@ -215,21 +215,21 @@ static void __init pci_mmconfig_remove(struct pci_mmcfg_region *cfg)
 	kfree(cfg);
 }
 
-void __init free_all_mmcfg(void)
+void __init pci_ecam_free_all(void)
 {
-	struct pci_mmcfg_region *cfg, *tmp;
+	struct pci_ecam_region *cfg, *tmp;
 
-	pci_mmcfg_arch_free();
-	list_for_each_entry_safe(cfg, tmp, &pci_mmcfg_list, list)
-		pci_mmconfig_remove(cfg);
+	pci_ecam_arch_free();
+	list_for_each_entry_safe(cfg, tmp, &pci_ecam_list, list)
+		pci_ecam_remove(cfg);
 }
 
-void list_add_sorted(struct pci_mmcfg_region *new)
+void pci_ecam_list_add_sorted(struct pci_ecam_region *new)
 {
-	struct pci_mmcfg_region *cfg;
+	struct pci_ecam_region *cfg;
 
 	/* keep list sorted by segment and starting bus number */
-	list_for_each_entry_rcu(cfg, &pci_mmcfg_list, list) {
+	list_for_each_entry_rcu(cfg, &pci_ecam_list, list) {
 		if (cfg->segment > new->segment ||
 		    (cfg->segment == new->segment &&
 		     cfg->start_bus >= new->start_bus)) {
@@ -237,13 +237,13 @@ void list_add_sorted(struct pci_mmcfg_region *new)
 			return;
 		}
 	}
-	list_add_tail_rcu(&new->list, &pci_mmcfg_list);
+	list_add_tail_rcu(&new->list, &pci_ecam_list);
 }
 
-struct pci_mmcfg_region *pci_mmconfig_alloc(int segment, int start,
+struct pci_ecam_region *pci_ecam_alloc(int segment, int start,
 					    int end, u64 addr)
 {
-	struct pci_mmcfg_region *new;
+	struct pci_ecam_region *new;
 	struct resource *res;
 
 	if (addr == 0)
@@ -259,29 +259,29 @@ struct pci_mmcfg_region *pci_mmconfig_alloc(int segment, int start,
 	new->end_bus = end;
 
 	res = &new->res;
-	res->start = addr + PCI_MMCFG_BUS_OFFSET(start);
-	res->end = addr + PCI_MMCFG_BUS_OFFSET(end + 1) - 1;
+	res->start = addr + PCI_ECAM_BUS_OFFSET(start);
+	res->end = addr + PCI_ECAM_BUS_OFFSET(end + 1) - 1;
 	res->flags = IORESOURCE_MEM | IORESOURCE_BUSY;
-	snprintf(new->name, PCI_MMCFG_RESOURCE_NAME_LEN,
-		 "PCI MMCONFIG %04x [bus %02x-%02x]", segment, start, end);
+	snprintf(new->name, PCI_ECAM_RESOURCE_NAME_LEN,
+		 "PCI ECAM %04x [bus %02x-%02x]", segment, start, end);
 	res->name = new->name;
 
 	return new;
 }
 
-struct pci_mmcfg_region *pci_mmconfig_add(int segment, int start,
+struct pci_ecam_region *pci_ecam_add(int segment, int start,
 					  int end, u64 addr)
 {
-	struct pci_mmcfg_region *new;
+	struct pci_ecam_region *new;
 
-	new = pci_mmconfig_alloc(segment, start, end, addr);
+	new = pci_ecam_alloc(segment, start, end, addr);
 	if (new) {
-		mutex_lock(&pci_mmcfg_lock);
-		list_add_sorted(new);
-		mutex_unlock(&pci_mmcfg_lock);
+		mutex_lock(&pci_ecam_lock);
+		pci_ecam_list_add_sorted(new);
+		mutex_unlock(&pci_ecam_lock);
 
 		pr_info(PREFIX
-		       "MMCONFIG for domain %04x [bus %02x-%02x] at %pR "
+		       "ECAM for domain %04x [bus %02x-%02x] at %pR "
 		       "(base %#lx)\n",
 		       segment, start, end, &new->res, (unsigned long)addr);
 	}
@@ -289,11 +289,11 @@ struct pci_mmcfg_region *pci_mmconfig_add(int segment, int start,
 	return new;
 }
 
-struct pci_mmcfg_region *pci_mmconfig_lookup(int segment, int bus)
+struct pci_ecam_region *pci_ecam_lookup(int segment, int bus)
 {
-	struct pci_mmcfg_region *cfg;
+	struct pci_ecam_region *cfg;
 
-	list_for_each_entry_rcu(cfg, &pci_mmcfg_list, list)
+	list_for_each_entry_rcu(cfg, &pci_ecam_list, list)
 		if (cfg->segment == segment &&
 		    cfg->start_bus <= bus && bus <= cfg->end_bus)
 			return cfg;
@@ -301,39 +301,39 @@ struct pci_mmcfg_region *pci_mmconfig_lookup(int segment, int bus)
 	return NULL;
 }
 
-/* Delete MMCFG information for host bridges */
-int pci_mmconfig_delete(u16 seg, u8 start, u8 end)
+/* Delete ECAM information for host bridges */
+int pci_ecam_delete(u16 seg, u8 start, u8 end)
 {
-	struct pci_mmcfg_region *cfg;
+	struct pci_ecam_region *cfg;
 
-	mutex_lock(&pci_mmcfg_lock);
-	list_for_each_entry_rcu(cfg, &pci_mmcfg_list, list)
+	mutex_lock(&pci_ecam_lock);
+	list_for_each_entry_rcu(cfg, &pci_ecam_list, list)
 		if (cfg->segment == seg && cfg->start_bus == start &&
 		    cfg->end_bus == end) {
 			list_del_rcu(&cfg->list);
 			synchronize_rcu();
-			pci_mmcfg_arch_unmap(cfg);
+			pci_ecam_arch_unmap(cfg);
 			if (cfg->res.parent)
 				release_resource(&cfg->res);
-			mutex_unlock(&pci_mmcfg_lock);
+			mutex_unlock(&pci_ecam_lock);
 			kfree(cfg);
 			return 0;
 		}
-	mutex_unlock(&pci_mmcfg_lock);
+	mutex_unlock(&pci_ecam_lock);
 
 	return -ENOENT;
 }
 
-int pci_mmconfig_inject(struct pci_mmcfg_region *cfg)
+int pci_ecam_inject(struct pci_ecam_region *cfg)
 {
-	struct pci_mmcfg_region *cfg_conflict;
+	struct pci_ecam_region *cfg_conflict;
 	int err = 0;
 
-	mutex_lock(&pci_mmcfg_lock);
-	cfg_conflict = pci_mmconfig_lookup(cfg->segment, cfg->start_bus);
+	mutex_lock(&pci_ecam_lock);
+	cfg_conflict = pci_ecam_lookup(cfg->segment, cfg->start_bus);
 	if (cfg_conflict) {
 		if (cfg_conflict->end_bus < cfg->end_bus)
-			pr_info(FW_INFO "MMCONFIG for "
+			pr_info(FW_INFO "ECAM for "
 				"domain %04x [bus %02x-%02x] "
 				"only partially covers this bridge\n",
 				cfg_conflict->segment, cfg_conflict->start_bus,
@@ -342,17 +342,17 @@ int pci_mmconfig_inject(struct pci_mmcfg_region *cfg)
 		goto out;
 	}
 
-	if (pci_mmcfg_arch_map(cfg)) {
-		pr_warn("fail to map MMCONFIG %pR.\n", &cfg->res);
+	if (pci_ecam_arch_map(cfg)) {
+		pr_warn("fail to map ECAM %pR.\n", &cfg->res);
 		err = -ENOMEM;
 		goto out;
 	} else {
-		list_add_sorted(cfg);
-		pr_info("MMCONFIG at %pR (base %#lx)\n",
+		pci_ecam_list_add_sorted(cfg);
+		pr_info("ECAM at %pR (base %#lx)\n",
 			&cfg->res, (unsigned long)cfg->address);
 
 	}
 out:
-	mutex_unlock(&pci_mmcfg_lock);
+	mutex_unlock(&pci_ecam_lock);
 	return err;
 }
