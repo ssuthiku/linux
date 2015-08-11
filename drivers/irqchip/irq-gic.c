814a385e02246f551665ecd9a1d09d489682a04f
Suravee Suthikulpanit <Suravee.Suthikulpanit@amd.com>
2015-08-11 12:10:25 +0700

    gicv2m: Introducing gicv2m_acpi_init()
    This patch introduces gicv2m_acpi_init(), which uses information
    in MADT GIC MSI frames structure to initialize GICv2m driver.
    It also refactors gicv2m_init_one() to handle both DT and ACPI
    initialization path.
    
    Signed-off-by: Suravee Suthikulpanit <Suravee.Suthikulpanit@amd.com>

--- drivers/irqchip/irq-gic.c
+++ drivers/irqchip/irq-gic.c
@@ -1154,6 +1154,9 @@ static int __init gic_v2_acpi_init(struct acpi_subtable_header *header,
 	 */
 	gic_init_bases(0, -1, dist_base, cpu_base, 0, (void *)ACPI_IRQ_MODEL_GIC);
 
+	if (IS_ENABLED(CONFIG_ARM_GIC_V2M))
+		gicv2m_acpi_init(gic_data[0].domain);
+
 	acpi_set_irq_model(ACPI_IRQ_MODEL_GIC, ACPI_IRQ_MODEL_GIC,
 			   gic_acpi_gsi_desc_populate);
 	return 0;
